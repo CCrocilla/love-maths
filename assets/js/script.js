1567ca84fc1889e0de0921f2ada2562c32104dbb
CCrocilla <cla.crocilla@gmail.com>
2022-02-12 19:24:37 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,12 +37,41 @@ function runGame(gameType){
 
 }
 
+/**
+ * Check the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value); //Since it is an imput element we need to get the value.
+    let calculateAnswer = calculateCorrectAnswer();
 
+    let isCorrect = userAnswer === calculateAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}!`);
+    }
+    runGame(calculateAnswer[1]);
 }
 
-function calculateCorrectAnswer() {
 
+/**
+ * Get the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and retuns the correct answer.
+ * We will use the parseInt Fuction in order to get value as Integer
+ */
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
